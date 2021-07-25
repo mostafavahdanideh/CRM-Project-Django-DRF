223f394105c446dd525a15d1e2524fd85cedcf8b
mostafa-mvd <vahdanim77@gmail.com>
2021-07-26 01:16:33 +0430

    edited templates

--- crm_project/users/views.py
+++ crm_project/users/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.shortcuts import redirect
 from django.contrib.auth import views
 
 
 class LoginUsers(views.LoginView):
     template_name = 'login_form.html'
 
-    def get(self, request, *args: str, **kwargs):
-        if request.path == "/":
-            return redirect("expert:login")
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("organization:list")
         return super().get(request, *args, **kwargs)
+
+    def form_invalid(self, form):
+        messages.info(self.request, form.errors)
+        return super().form_invalid(form)
 
 
 class LogoutUsers(views.LogoutView):
