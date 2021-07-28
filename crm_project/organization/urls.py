4afb6a5cd5b0d9b94b6464ce458f5732e56a09ca
mostafa-mvd <vahdanim77@gmail.com>
2021-07-29 01:10:53 +0430

    added update functionality for organizations

--- crm_project/organization/urls.py
+++ crm_project/organization/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('register/', views.CreateOrganization.as_view(), name="register"),
     path('list/', views.ListOrganization.as_view(), name="list"),
     path("detail/<int:pk>/", views.DetailOrganization.as_view(), name='detail'),
+    path('update/', views.UpdateOrganization.as_view(), name='update'),
 ]
--- crm_project/organization/views.py
+++ crm_project/organization/views.py
@@ -1,7 +1,10 @@
+from django.http.response import Http404, JsonResponse
+from django.shortcuts import get_object_or_404
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib import messages
 from . import forms, models
+from inventory import models as inventory_models
 
 
 class CreateOrganization(generic.CreateView):
@@ -36,3 +39,56 @@ class ListOrganization(generic.ListView):
 class DetailOrganization(generic.DetailView):
     template_name = 'organization_details.html'
     model = models.Organization
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        organization_object = context['object']
+        qs_province = models.Province.objects.exclude(name=organization_object.province)
+        context['provinces'] = qs_province
+        return context
+
+
+class UpdateOrganization(generic.UpdateView):
+    queryset = models.Organization.objects.all()
+    fields = (
+        'province',
+        'organization_phone_number',
+        'workers_size',
+        'owner_phone_number',
+        'owner_first_name',
+        'owner_last_name',
+        'owner_second_last_name',
+        'owner_email',
+    )
+
+    def get_object(self, queryset=None):
+        pk = self.request.GET.get("pk", None)
+        if pk:
+            if pk.isdigit():
+                user = self.request.user
+                organization_obj = get_object_or_404(klass=models.Organization, pk=pk)
+
+                if (user == organization_obj.expert_creator) or (user.is_superuser):
+                    return organization_obj
+                else:
+                    raise Http404
+            else:
+                raise Http404
+        else:
+            raise Http404
+    
+    def form_valid(self, form):
+        form.save()
+
+        return JsonResponse(
+            {
+                'messages': "You've successfully updated",
+            }, status=200
+        )
+    
+    def form_invalid(self, form):
+        return JsonResponse(
+            {
+                'messages': form.errors,
+            }, status=400
+        )
