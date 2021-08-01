68a4a0d35f7d3c1c3fb432d94e2c2b79b76eca2e
mostafa-mvd <vahdanim77@gmail.com>
2021-08-01 22:56:22 +0430

    added code for show list, detail and download of quotes

--- crm_project/marketing/urls.py
+++ crm_project/marketing/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('follow-up-history/<int:pk>/', views.OrganizationFollowUpHistory.as_view(), name='follow_up_history'),
     path('create-quote/', views.CreateQuotes.as_view(), name='create_quote'),
     path('list-quote/', views.ListQuotes.as_view(),name='list_quotes'),
+    path('detail-quote/<int:pk>/', views.DetailQuotes.as_view(),name='detail_quote'),
+    path('download-quote/<int:pk>/', views.DownloadDetailQuote.as_view(), name='download-quote-pdf'),
 ]
--- crm_project/marketing/views.py
+++ crm_project/marketing/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import get_object_or_404, render
+from django.contrib import messages
+from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth import mixins
 from django.views import generic
-from . import models
+from . import models, forms
 from organization import models as org_models
+import weasyprint
 
 
 class OrganizationFollowUpHistory(generic.ListView):
@@ -25,12 +28,59 @@ class OrganizationFollowUpHistory(generic.ListView):
         return context
 
 
-
 class CreateQuotes(mixins.LoginRequiredMixin, generic.CreateView):
     template_name = 'create_quotes.html'
-    model = models.Quote
+    model = models.QuoteItem
+    form_class = forms.AddQuoteItemsForm
+
+    def form_invalid(self, form):
+        messages.info(self.request, form.errors)
+        return super().form_invalid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['organizations'] = org_models.Organization.objects.all()
+        return context
 
 
 class ListQuotes(mixins.LoginRequiredMixin, generic.ListView):
     template_name = 'quotes_list.html'
     model = models.Quote
+    paginate_by = 3
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        if not self.request.user.is_superuser:
+            qs = qs.filter(owner__expert_creator=self.request.user)
+
+        return qs
+
+
+class DetailQuotes(mixins.LoginRequiredMixin, generic.DetailView):
+    template_name = 'detail_quote.html'
+    model = models.Quote
+
+    def quotes_for_user_exists(self):
+        qs =  self.get_queryset()
+        exists = qs.filter(owner__expert_creator=self.request.user).filter(pk=self.kwargs.get('pk', None)).exists()
+        return exists
+
+    def get(self, request, *args, **kwargs):
+        if self.quotes_for_user_exists() or self.request.user.is_superuser:
+            return super().get(request, *args, **kwargs)
+        else:
+            return redirect("marketing:list_quotes")
+
+
+class DownloadDetailQuote(mixins.LoginRequiredMixin, generic.DetailView):
+    template_name = 'generate_quote_pdf.html'
+    model = models.Quote
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        content = response.rendered_content
+        pdf = weasyprint.HTML(string=content, base_url='http://127.0.0.1:8000').write_pdf()
+        pdf_response = HttpResponse(content=pdf, content_type='application/pdf')
+        # pdf_response['Content-Disposition'] = 'filename="quote_detail.pdf"'
+        return pdf_response
