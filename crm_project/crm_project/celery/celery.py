46fc818252417c5ced167fe8b41be71ae3e050d3
mostafa-mvd <vahdanim77@gmail.com>
2021-08-04 00:50:28 +0430

    edited celery confing to django

--- crm_project/crm_project/celery/celery.py
+++ crm_project/crm_project/celery/celery.py
@@ -7,3 +7,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm_project.settings')
 app = Celery('crm_project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
